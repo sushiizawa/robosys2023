d034561de660ae8b7e8c9f045229d9dfd305c711
Sushi Izawa <izayu4649@gmail.com>
2023-11-02 12:10:36 +0900

    change

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightTest: 2023 sushi izawa
+# SPDX-LICENSE-Idensifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/0 test ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
